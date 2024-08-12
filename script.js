9dd7a2329768134837a93e808fd2fd4d769fe34f
Lanrecode23 <lanrecode23@gmail.com>
2024-08-12 13:17:13 +0100

    changes added

---
+++ script.js
@@ -0,0 +1,11 @@
+const contactForm = document.getElementById('contactForm');
+const email = document.getElementById('Email');
+const text = document.getElementById('Message');
+
+contactForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    if (email.value === "" || text.value === "") {
+        alert('fill in the blank space');
+    }
+});
